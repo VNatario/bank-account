936445c42942c1bf9a784d789bffb5d6bc4df005
Vitor Natario <natariovitor@gmail.com>
2023-10-28 00:18:04 -0300

    initial commit and add TODO

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+/*
+* TODO
+*   -Inicializar dados do cliente
+*   -Menu de opções
+*   -Visualizar saldo
+*   -Enviar Valor
+*       -Max. valor da conta
+*   -Receber Valor
+*       -Somente valor positivo
+*
+* */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
