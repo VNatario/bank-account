62530f790048961ae05c1a0aa67d1602c7db1c7e
Vitor Natario <natariovitor@gmail.com>
2023-10-28 00:22:44 -0300

    feat: display customer data

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,16 @@ public class Main {
 *
 * */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("""
+                ********************************
+                Dados iniciais do cliente:
+                
+                Nome:             Vitor Natario
+                Tipo conta:       Corrente
+                Saldo inicial:    R$ 3000,00
+                ********************************
+                """);
+
+
     }
 }
